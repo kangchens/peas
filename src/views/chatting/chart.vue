5fddc952d5e1eb2407743bd668543057621f2e07
陈康 <chenkang@vip56.cn>
2020-05-26 17:31:07 +0800

    change

---
+++ src/views/chatting/chart.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang='ts'>
+    import { Component, Prop, Vue } from "vue-property-decorator";  
+    import { State, Action, Mutation } from "vuex-class";
+    
+    
+</script>
+
+<style lang="less" scoped>
+
+</style>
